7c03382945ab0e60abb415c46e1fe4c1e7bee8c4
voidbreath <tinselfairy1@gmail.com>
2025-11-06 09:55:46 +0000

    added var roleSupplier

--- Runtime/main.js
+++ Runtime/main.js
@@ -2,6 +2,7 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
+var roleSupplier = require('role.supplier');
 
 module.exports.loop = function () {
 
